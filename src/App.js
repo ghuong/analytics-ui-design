853c031def7a7478ec38543568c62b6745f4c0cf
Gary Huong <garyhuong@gmail.com>
2022-05-30 17:42:11 -0400

    Remove console.log

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import MetricsView from "./components/MetricsView";
 
 function App() {
   const metrics = getAllMetrics();
-  console.log(metrics);
 
   return (
     <div className="min-h-screen flex justify-center items-center bg-gray_custom">
